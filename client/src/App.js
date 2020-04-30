d305c31e5afc7ef97825e9703a7126583f0e4c5c
naman159 <namanranawat159@gmail.com>
2020-04-30 14:40:53 +0530

    First

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,35 @@
-import React from 'react';
+import React, { Component } from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <h1>Yo!</h1>
-  );
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <section>
+          <header>
+            <img src="./leaf.png" width="100" alt="logo" />
+            <div>
+              <ul>
+                <li>HOME</li>
+                <li>ABOUT</li>
+                <li>SERVICES</li>
+                <li>BLOG</li>
+              </ul>
+            </div>
+          </header>
+        </section>
+
+        <section id="main">
+          <div className="main-text">
+            <span>kindself.</span> <br /> where we reinvent <br />
+            the meaning of selfcare
+          </div>
+
+          <img src="./images/leaf.png" width="520" alt="leaf-main-image" />
+        </section>
+      </div>
+    );
+  }
 }
 
 export default App;
