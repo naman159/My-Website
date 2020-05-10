6c6f9fc9a671db0990c46c6ea7e3faa625e7fb49
naman159 <namanranawat159@gmail.com>
2020-05-10 13:51:49 +0530

    Code tab added

--- src/components/navs/code/code.js
+++ src/components/navs/code/code.js
@@ -1,13 +1,37 @@
 import React, { Component } from "react";
 import Typical from 'react-typical';
+import Fade from 'react-reveal/Fade';
 
 class Code extends Component {
   render() {
     return (
-      <div className="mt2">
-        <div>
-          <h4 className="tc neon">Welp, not sure what to do here XD. </h4>
-        </div>
+      <div>
+      <div>
+      <Fade big>
+        <p className="tc">
+          <span className="notneon">I have worked with a few technologies.</span>
+        </p>
+      </Fade>
+      </div>
+        <Fade big>
+          <div className="br2 ba bw1 bc mv3 ph2 shadow-2 pa1 codediv">
+            <p className="tl">
+              <span className="mr1 neon">▹ </span>
+              <span className="notneon mr2">Programming Languages :</span>
+              <span>Java, Python, JavaScript, C</span>
+            </p>
+            <p className="tl">
+              <span className="mr1 neon">▹ </span>
+              <span className="notneon mr2">Web Technologies :</span>
+              <span>HTML & CSS, React, Node.js, Express.js</span>
+            </p>
+            <p className="tl">
+              <span className="mr1 neon">▹ </span>
+              <span className="notneon mr2">Python Scientific Libraries :</span>
+              <span>Numpy, Pandas, Matplotlib, Plotly, Scikit Learn, Tensorflow & Keras, Pytorch, Fast.ai</span>
+            </p>
+          </div>
+        </Fade>
       </div>
     );
   }
