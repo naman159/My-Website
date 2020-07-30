ac3a4d11f446d995760297c26ef1d0ed98788fe4
naman159 <namanranawat159@gmail.com>
2020-07-30 12:01:03 +0530

    Jikan servers glitchy, code fine

--- server/server.js
+++ server/server.js
@@ -1,8 +1,8 @@
-var express = require('express');
-var request = require('request');
-var cheerio = require('cheerio');
-const bodyParser = require('body-parser');
-var cors = require('cors');
+var express = require("express");
+var request = require("request");
+var cheerio = require("cheerio");
+const bodyParser = require("body-parser");
+var cors = require("cors");
 var app = express();
 
 app.use(cors());
@@ -11,39 +11,80 @@ app.use(bodyParser.json());
 
 const PORT = process.env.PORT || 3001;
 
-app.get('/manga', function(req, response){
-	//Web Scraping
- 	  url = "https://www.anime-planet.com/users/kyoharato"
- 	  manga = request(url, function(err, res, html) {
-  	//If statement to check for no errors
-  	if(!err){
+app.get("/manga", function(req, response) {
+  //Web Scraping
+  url = "https://www.anime-planet.com/users/kyoharato";
+  manga = request(url, function(err, res, html) {
+    //If statement to check for no errors
+    if (!err) {
       //Cheerio Library gives jQuery functionality
       const $ = cheerio.load(html);
-      var json = { read : "", reading : "", wtr : "", stalled: "", dropped: "", ts: ""};
+      var json = {
+        read: "",
+        reading: "",
+        wtr: "",
+        stalled: "",
+        dropped: "",
+        ts: ""
+      };
       var str = "";
-      json.read = $('.status1').last().children().children().first().text();
-      json.reading = $('.status2').last().children().children().first().text();
-      json.dropped = $('.status3').last().children().children().first().text();
-      json.wtr = $('.status4').last().children().children().first().text();
-      json.stalled = $('.status5').last().children().children().first().text()
-      var tr = $('.pure-1-6').each(function(){
- 				str += $(this).text() + " ";
- 			});
- 			str = str.split(" ");
+      json.read = $(".status1")
+        .last()
+        .children()
+        .children()
+        .first()
+        .text();
+      json.reading = $(".status2")
+        .last()
+        .children()
+        .children()
+        .first()
+        .text();
+      json.dropped = $(".status3")
+        .last()
+        .children()
+        .children()
+        .first()
+        .text();
+      json.wtr = $(".status4")
+        .last()
+        .children()
+        .children()
+        .first()
+        .text();
+      json.stalled = $(".status5")
+        .last()
+        .children()
+        .children()
+        .first()
+        .text();
+      var tr = $(".pure-1-6").each(function() {
+        str += $(this).text() + " ";
+      });
+      str = str.split(" ");
       var i;
       var arr = [];
-      for(i = 0; i<(str.length-1); i++) {
-    	   if(!isNaN(str[i])){
-    		     arr.push(parseInt(str[i], 10));
-    	   }
+      for (i = 0; i < str.length - 1; i++) {
+        if (!isNaN(str[i])) {
+          arr.push(parseInt(str[i], 10));
+        }
       }
-      json.ts = (1+arr[7]+arr[8]*24+arr[9]*7*24+arr[10]*30*24+arr[11]*365*24).toString();
+      json.ts = (
+        1 +
+        arr[7] +
+        arr[8] * 24 +
+        arr[9] * 7 * 24 +
+        arr[10] * 30 * 24 +
+        arr[11] * 365 * 24
+      ).toString();
       response.send(json);
     }
-  })
+  });
 });
 
-app.listen(PORT)
+app.get("/anime", function(req, response) {});
+
+app.listen(PORT);
 
 console.log(`Running on port ${PORT}`);
 
--- src/components/home.js
+++ src/components/home.js
@@ -1,107 +1,154 @@
 import React, { Component } from "react";
 import "./home.css";
 import Head from "./head";
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import axios from 'axios'
-const jikanjs = require('jikanjs');
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import axios from "axios";
+const jikanjs = require("jikanjs");
 
 class Home extends Component {
-
-  constructor(props){
+  constructor(props) {
     super(props);
     this.state = {
-      anime:'',
-      manga: '',
-      color1:'notneonn',
-      color2:'notneonn',
-      color3:'notneonn',
-      color4:'notneonn',
-    }
+      anime: "",
+      manga: "",
+      color1: "notneonn",
+      color2: "notneonn",
+      color3: "notneonn",
+      color4: "notneonn"
+    };
   }
 
-  componentDidMount(){
-    jikanjs.loadUser('kyoharato', 'profile').then((response) => {
-      this.setState({anime: response.anime_stats});
-      console.log(response);
-    })
-    .catch((err) => {
-      console.error(err);
-    })
+  componentDidMount() {
+    jikanjs.settings.version = 3;
+    jikanjs
+      .loadUser("kyoharato", "profile")
+      .then(response => {
+        this.setState({ anime: response.anime_stats });
+        //console.log(response);
+      })
+      .catch(err => {
+        console.error(err);
+      });
 
     const url = "https://namanranawat.herokuapp.com/manga";
 
-    axios.get(url)
-        .then(res => {
-          //Set state with result
-          console.log(res.data);
-          this.setState({ manga: res.data });
-        });
-
+    axios.get(url).then(res => {
+      //Set state with result
+      //console.log(res.data);
+      this.setState({ manga: res.data });
+    });
   }
 
   render() {
     return (
       <div className="flex flex-wrap">
+        <div className="tr">
+          <div className="flex flex-column">
+            <div className="isize pa2">
+              <a
+                style={{ display: "table-cell" }}
+                href="https://www.github.com/naman159"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseOver={() => {
+                  this.setState({ color1: "rainbow neon grow" });
+                }}
+                onMouseOut={() => {
+                  this.setState({ color1: "notneonn grow" });
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={["fab", "github"]}
+                  className={this.state.color1}
+                />
+              </a>
+            </div>
 
-      <div className="tr">
-      <div className="flex flex-column">
+            <div className="isize pa2">
+              <a
+                style={{ display: "table-cell" }}
+                href="https://www.linkedin.com/in/nranawat/"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseOver={() => {
+                  this.setState({ color2: "rainbow neon grow" });
+                }}
+                onMouseOut={() => {
+                  this.setState({ color2: "notneonn grow" });
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={["fab", "linkedin"]}
+                  className={this.state.color2}
+                />
+              </a>
+            </div>
 
-      <div className="isize pa2">
-        <a style={{display: "table-cell"}} href="https://www.github.com/naman159" target="_blank" rel="noopener noreferrer"
-          onMouseOver={() => { this.setState({color1: "rainbow neon grow"})}}
-          onMouseOut={() => { this.setState({color1: "notneonn grow"})}}
-          >
-          <FontAwesomeIcon icon={['fab', 'github']} className={this.state.color1}/>
-        </a>
-      </div>
+            <div
+              className="isize pa2"
+              onMouseOver={e => {
+                e.target.style.color3 = "#64FFDA";
+              }}
+              onMouseOut={e => {
+                e.target.style.color3 = "#8892B0";
+              }}
+            >
+              <a
+                style={{ display: "table-cell" }}
+                href="https://www.instagram.com/trademarkedsetups/"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseOver={() => {
+                  this.setState({ color3: "rainbow neon grow" });
+                }}
+                onMouseOut={() => {
+                  this.setState({ color3: "notneonn grow" });
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={["fab", "instagram"]}
+                  className={this.state.color3}
+                />
+              </a>
+            </div>
 
-        <div className="isize pa2">
-          <a style={{display: "table-cell"}} href="https://www.linkedin.com/in/nranawat/" target="_blank" rel="noopener noreferrer"
-          onMouseOver={() => { this.setState({color2: "rainbow neon grow"})}}
-          onMouseOut={() => { this.setState({color2: "notneonn grow"})}}
-          >
-            <FontAwesomeIcon icon={['fab', 'linkedin']} className={this.state.color2}/>
-          </a>
+            <div
+              className="isize pa2"
+              onMouseOver={e => {
+                e.target.style.color4 = "#64FFDA";
+              }}
+              onMouseOut={e => {
+                e.target.style.color4 = "#8892B0";
+              }}
+            >
+              <a
+                style={{ display: "table-cell" }}
+                href="https://twitter.com/naman1591"
+                target="_blank"
+                rel="noopener noreferrer"
+                onMouseOver={() => {
+                  this.setState({ color4: "rainbow neon grow" });
+                }}
+                onMouseOut={() => {
+                  this.setState({ color4: "notneonn grow" });
+                }}
+              >
+                <FontAwesomeIcon
+                  icon={["fab", "twitter"]}
+                  className={this.state.color4}
+                />
+              </a>
+            </div>
+          </div>
         </div>
-
-        <div className="isize pa2"
-          onMouseOver={(e) => { e.target.style.color3 = '#64FFDA' }}
-          onMouseOut={(e) => { e.target.style.color3 = '#8892B0' }}>
-          <a style={{display: "table-cell"}} href="https://www.instagram.com/trademarkedsetups/" target="_blank" rel="noopener noreferrer"
-          onMouseOver={() => { this.setState({color3: "rainbow neon grow"})}}
-          onMouseOut={() => { this.setState({color3: "notneonn grow"})}}
-          >
-          <FontAwesomeIcon icon={['fab', 'instagram']} className={this.state.color3}/>
-          </a>
-        </div>
-
-        <div className="isize pa2"
-          onMouseOver={(e) => { e.target.style.color4 = '#64FFDA' }}
-          onMouseOut={(e) => { e.target.style.color4 = '#8892B0' }}>
-          <a style={{display: "table-cell"}} href="https://twitter.com/naman1591" target="_blank" rel="noopener noreferrer"
-          onMouseOver={() => { this.setState({color4: "rainbow neon grow"})}}
-          onMouseOut={() => { this.setState({color4: "notneonn grow"})}}
-          >
-          <FontAwesomeIcon icon={['fab', 'twitter']} className={this.state.color4}/>
-          </a>
-        </div>
-
-      </div>
-      </div>
-
 
         <div className="center">
-          <Head anime={this.state.anime} manga={this.state.manga}/>
+          <Head anime={this.state.anime} manga={this.state.manga} />
         </div>
 
         <div className="tr">
-        <div className="flex flex-column isize pa2">
-
+          <div className="flex flex-column isize pa2"></div>
         </div>
-        </div>
-
-
-
       </div>
     );
   }
